8e2f681d739e41f9e119f3fbb2798756ceb63223
5id5hah <157269117+5id5hah@users.noreply.github.com>
2024-07-27 11:53:27 +0530

    Add files via upload

---
+++ Recursive_SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Recursive_SelectionSort {
+        public static void main(String[] args) {
+            int[] arr = {4,3,8,7,6,9,1,2};
+            Selection(arr,arr.length,0,0);
+            System.out.println(Arrays.toString(arr));
+
+        }
+        public static void Selection(int[] arr, int r, int c, int max){
+            if(r == 0){
+                return;
+            }
+            if (c < r){
+                if (arr[c] > arr[max]) {
+                   Selection(arr,r,c+1,c);
+                }else {
+                Selection(arr, r, c+1,max);
+                }
+            }
+
+
+
+            else {
+                int temp = arr[max];
+                arr[max] = arr[r-1];
+                arr[r-1] = temp;
+                Selection(arr,r-1,0,0);
+            }
+        }
+}
+
